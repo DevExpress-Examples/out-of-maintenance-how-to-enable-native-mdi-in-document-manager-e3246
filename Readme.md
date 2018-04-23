84a96a41428740321c2b0088695946abdb1542ec
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to: Enable Native MDI in Document Manager
+
+
+<p>This example shows how to enable a native MDI for a DocumentManager where MDI child windows are presented as regular windows within a container.</p><br />
+
+
+<br/>
+
+
---
+++ VB/DocumentManager_NativeMDI/Form1.vb
@@ -0,0 +1,45 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraBars.Docking2010
+Imports DevExpress.XtraBars.Docking2010.Views.NativeMdi
+Imports DevExpress.XtraEditors
+
+Namespace DocumentManager_NativeMDI
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private childCount As Integer = 0
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			CreateDocumentManager()
+			For i As Integer = 0 To 2
+				AddChild()
+			Next i
+		End Sub
+		Private Sub CreateDocumentManager()
+			Dim dm As New DocumentManager()
+			dm.MdiParent = Me
+			dm.View = New NativeMdiView()
+		End Sub
+		Private Sub AddChild()
+			Dim childForm As Form = Nothing
+			childForm = New Form()
+			childCount += 1
+			childForm.Text = "Child Form " & (childCount)
+
+			Dim btn As New SimpleButton()
+			btn.Text = "Button " & childCount
+			btn.Parent = childForm
+
+			childForm.MdiParent = Me
+			childForm.Show()
+		End Sub
+	End Class
+End Namespace
